9d75e80abbe5333b74e7ee49fb91488a46e57989
volodymyr bondariev <vladmikhbond@hotmail.com>
2019-08-03 12:43:27 +0300

    half ready

--- src/app/picture.component.ts
+++ src/app/picture.component.ts
@@ -51,22 +51,60 @@ export class PictureComponent {
 
     const canvas = this.canvas.nativeElement;
     const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = this.getInfitityColor();
     ctx.fillRect(0, 0, canvas.width, canvas.height );
+
     // const low = this.win.minIter(canvas);
     for (let x = 0; x < canvas.width; x += D) {
       for (let y = 0; y < canvas.height; y += D) {
         const [wx, wy] = this.win.canvasToWorld(x, y);
         const count = this.win.countIter(wx, wy);
         if (count < this.win.iterLimit) {
-          ctx.fillStyle = 'white'; // getColor(count, low);
+          ctx.fillStyle = this.getColor(count);
           ctx.fillRect(x, y, D, D);
         }
       }
     }
     this.elapsedTime = new Date().valueOf() - t.valueOf();
     console.log(this.elapsedTime + ' msec');
+
   }
 
 
+  private getInfitityColor() {
+    switch (this.colorSchema.schemaNo) {
+      case 0: case 1: case 2: case 3:
+        return this.colorSchema.dark;
+    }
+  }
+
+  private getColor(n: number, low: number = 0) {
+    const fiery = ['red', 'vermilion', 'orange', 'amber', 'yellow',
+      'chartreuse', 'green', 'teal', 'blue', 'violet', 'purple', 'magenta'];
+    const zebra = [this.colorSchema.light, this.colorSchema.third];
+
+    switch (this.colorSchema.schemaNo) {
+      case 0:
+        return this.colorSchema.light;
+      case 1:
+        const i = (n % fiery.length);
+        return fiery[i];
+      case 2:
+        return zebra[n % 2];
+      case 3:
+        const k = (n - low) / (this.win.iterLimit - low);
+        const r1 = parseInt((this.colorSchema.light.substr(1, 2)), 16);
+        const g1 = parseInt((this.colorSchema.light.substr(3, 2)), 16);
+        const b1 = parseInt((this.colorSchema.light.substr(5, 2)), 16);
+        const r2 = parseInt((this.colorSchema.third.substr(1, 2)), 16);
+        const g2 = parseInt((this.colorSchema.third.substr(3, 2)), 16);
+        const b2 = parseInt((this.colorSchema.third.substr(5, 2)), 16);
+        const r = Math.trunc(r1 * k + r2 * (1 - k));
+        const g = Math.trunc(g1 * k + g2 * (1 - k));
+        const b = Math.trunc(b1 * k + b2 * (1 - k));
+        return `rgb(${r}, ${g}, ${b})`;
+
+    }
+  }
 }
+
